09a8f19e140cc89c881e3af313611f355e2e9dfc
raziel rodrigues <raziel@upoutsourcing.com>
2020-09-25 22:02:06 -0300

    creating a form

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP SEARCH ENGINE PROJECT</title>
+</head>
+<body>
+    <form action="result.php" method="post">
+
+        <input type="text" name="userQuery" id="userQuery">
+        <input type="submit" name="searchInput"value="Search now">
+    
+    </form>
+</body>
+</html>
